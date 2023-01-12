3efc79bfba05fecf5d1b05d9f27e2f594067f33d
Štěpán Marousek <71834145+1000101cz@users.noreply.github.com>
2023-01-12 22:56:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 document.querySelector("#dynamic-content").innerText =
- "This paragraph is dynamically rendered using JavaScript";
+ "Welcome to my ugly personal website running on ICP blockchain\n" + 
+ "- last update: 2023/01/12\n" + 
+"\n" + 
+ "About me:\n" + 
+ "- I'm computer science student interested in blockchains and machine learning.\n" + 
+ "- skills: Python, C \n" + 
+ "- and also: Rust, Java, VHDL, Cisco IOS, ...\n" +
+ "\n" + 
+ "Experience:\n" + 
+ "- Data Analyst - Robert Bosch s.r.o. (September 2022 - present)\n" + 
+ "- - - - - development of production anomaly detection using neural networks\n" + 
+ "- - - - - Python, NumPy, PyQT, TensorFlow, ...\n" + 
+ "\n" + 
+ "Education:\n" + 
+ "- FEE CTU in Prague (September 2020 - present)\n" + 
+ "- - - - - Cybernetics and Robotics (2020 - 2022)\n" + 
+ "- - - - - Open Informatics: Internet of Things (2022 - present)\n" + 
+ "\n\n\n\n\n\n\n" + 
+ "If you're feeling generous, you can support my education\n" + 
+ "- SOL: 2Zyy3BQAmZP1XZ7u1HRCo9d1tEawvLMKUTJi8ttJztpR\n" + 
+ "- ATOM: cosmos1l7sx2d5d5w4tkjd4w8q634zvq4fy9uhny6amet\n" + 
+ "- TRX: TXc58RCWsoc9yHJP5KB5Y1brZQ5JYVXLaP\n" + 
+ "- ICP: 770eb96085e3da3f13359ed815983e75afa961042fbbe2afdd2e6dea9609667d\n" + 
+ "\n" + 
+ ":)\n";
